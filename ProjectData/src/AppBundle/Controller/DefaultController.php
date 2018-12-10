9def7ecbf09bc094f2fa996decd720dacdac092c
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-12-10 18:18:34 +0100

    true trying first absence form

--- ProjectData/src/AppBundle/Controller/DefaultController.php
+++ ProjectData/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Absence;
+use AppBundle\Form\AbsenceType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,4 +22,19 @@ class DefaultController extends Controller
         return $this->render('@App/index.html.twig');
         // replace this example code with whatever you need0
     }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAbsenceAction(Request $request)
+    {
+        $absence = new Absence();
+        $form = $this->createForm(AbsenceType::class, $absence);
+
+        return $this->render('@App/Absence/index.html.twig',array(
+            'absence_form' => $form->createView()
+        ));
+        // replace this example code with whatever you need0
+    }
+
 }
