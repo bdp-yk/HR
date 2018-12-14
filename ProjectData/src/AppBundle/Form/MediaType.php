e2c8de040926871ee39dc154c99261765ed40e50
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-12-14 13:24:39 +0100

    20

--- ProjectData/src/AppBundle/Form/MediaType.php
+++ ProjectData/src/AppBundle/Form/MediaType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class MediaType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('type')->add('source')->add('candidat_proprietaire')->add('media_offre')->add('absence')->add('media_event');
+        $builder->add('type')->add('source')->add('candidat_proprietaire')->add('media_offre',FileType::class)->add('absence')->add('media_event');
     }/**
      * {@inheritdoc}
      */
